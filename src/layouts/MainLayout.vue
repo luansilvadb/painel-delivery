e674362060889608bf3515ff96c2fb319b0b0da0
luansilvadb <luan.videira@gmail.com>
2024-01-20 22:48:00 -0300

    layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,102 +1,89 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+  <q-layout view="lhr lpr lfr">
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
+    <q-drawer style="overflow-x: hidden;" show-if-above v-model="drawer" side="left" :width="200"
+      :mini="!drawer || miniState" @click.capture="drawerClick">
+      <div class="logo-header">
+        <img src="~assets/empresa-logo-vertical.svg" />
+      </div>
 
-        <div>Quasar v{{ $q.version }}</div>
+      <q-list style="opacity:90% ;">
+        <q-item v-for="item in menuItems" :key="item.label" clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon :name="item.icon" />
+          </q-item-section>
+          <q-item-section>{{ item.label }}</q-item-section>
+        </q-item>
+      </q-list>
+
+      <div class="drawer-footer">
+        <q-btn dense flat round @click="miniState = true">
+          <q-avatar size="14px">
+            <img src="src\assets\right-arrow.svg" />
+          </q-avatar>
+        </q-btn>
+      </div>
+    </q-drawer>
+
+    <q-header reveal class="bg-white text-white">
+      <q-toolbar style="padding: 10px 0px 30px 20px;">
+        <q-input rounded type="search" style="height: 20px; width: 500px; " outlined
+          placeholder="Search by Store ID, E-mail, Keyword" dense>
+          <template v-slot:append>
+            <q-icon name="search" />
+          </template>
+        </q-input>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
-      <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
-    <q-page-container>
+    <q-page-container style="background-color: #F5F5F5;">
       <router-view />
     </q-page-container>
   </q-layout>
 </template>
 
-<script setup lang="ts">
+<script setup>
 import { ref } from 'vue';
-import EssentialLink, { EssentialLinkProps } from 'components/EssentialLink.vue';
 
-const essentialLinks: EssentialLinkProps[] = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
+const miniState = ref(false);
+const drawer = ref(false);
+
+const drawerClick = (e) => {
+  if (miniState.value) {
+    miniState.value = false;
+    e.stopPropagation();
   }
+};
+
+const menuItems = [
+  { label: 'Dashboard', icon: 'speed' },
+  { label: 'Orders', icon: 'restaurant_menu' },
+  { label: 'Users', icon: 'people' },
+  { label: 'Products', icon: 'shopping_cart' },
+  { label: 'Stores', icon: 'store' },
+  { label: 'Categories', icon: 'category' },
+  { label: 'Couriers', icon: 'local_shipping' },
+  { label: 'Reviews', icon: 'try' },
+  { label: 'Logout', icon: 'logout' },
 ];
-
-const leftDrawerOpen = ref(false)
-
-function toggleLeftDrawer() {
-  leftDrawerOpen.value = !leftDrawerOpen.value
-}
 </script>
+
+<style scoped>
+.logo-header {
+  padding: 10px;
+  height: 64px;
+  display: flex;
+  align-items: center;
+}
+
+.drawer-footer {
+  position: fixed;
+  bottom: 0;
+  z-index: 1;
+  height: 48px;
+  color: #000000;
+  line-height: 48px;
+  text-align: center;
+  width: 100%;
+}
+</style>
